fec2ae3a813226a39b2693fb8f828c17fd9e3e4d
ping.guan <ping.guan@cyber-insight.com>
2021-06-22 22:26:59 +0800

    test

--- src/Upload.jsx
+++ src/Upload.jsx
@@ -2,6 +2,8 @@ import { Upload, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { useState } from 'react'
 
+// 资料 https://blog.csdn.net/weixin_43249665/article/details/100084637
+
 const UploadList = () => {
   const [fileList, setFileList] = useState([])
   const props = {
@@ -25,10 +27,10 @@ const UploadList = () => {
     })
     console.log('=====', formData.getAll('files')) // 获取到文件列表
     console.log('444444', formData.get('files'))  // 只能获取到一个文件
+    console.log('-----', formData.entries('key'))
+
 
     const res = await fetch('https://www.mocky.io/v2/5cc8019d300000980a055e76', { method: 'POST', body: formData }).then(res => res.json)
-
-    console.log('-----', formData)
   }
 
   return <><Upload {...props} directory>
